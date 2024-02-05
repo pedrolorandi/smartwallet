05e7b3a2c3eed7062bc82da98b926744a196931c
pedrolorandi <pedrokl@hotmail.com>
2024-02-05 16:34:10 -0500

    install axios

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,25 @@
-import "./App.css";
+import axios from "axios";
+
+const NODE_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+
+// TODO: Move this function to a separate file
+function navigate(url) {
+  window.location.href = url;
+}
+
+async function auth() {
+  const response = await axios.post(`${NODE_BASE_URL}/request`);
+  navigate(response.data.url);
+}
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <button type="button" onClick={() => auth()}>
+        Sign In with Google
+      </button>
+    </div>
+  );
 }
 
 export default App;
