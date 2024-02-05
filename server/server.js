bcee8bae34246182ce910de24ab171854a66b22c
pedrolorandi <pedrokl@hotmail.com>
2024-02-05 15:10:03 -0500

    Install dotenv

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
